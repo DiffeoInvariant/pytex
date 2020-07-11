340c0b215adfeb3b59048f2400c601f6f3cd8165
DiffeoInvariant <zane.jakobs@colorado.edu>
2020-07-11 14:01:17 -0700

    LaTeX Equation environment from FEniCS UFL

--- pytex/environment.py
+++ pytex/environment.py
@@ -73,6 +73,148 @@ class Environment(TextLines):
         self.lines = self.lines[1:]
         self.lines[0] = self.begin if self.begin.endswith('\n') else self.begin+'\n'
 
+
+unicode_supers = {'\u2070'           : '^0',
+                  f'\N{DEGREE SIGN}' : '^0',
+                  f'\N{SUPERSCRIPT ONE}' : '^1',
+                  f'\N{SUPERSCRIPT TWO}' : '^2',
+                  f'\N{SUPERSCRIPT THREE}': '^3',
+                  f'\N{SUPERSCRIPT FOUR}': '^4',
+                  f'\N{SUPERSCRIPT FIVE}': '^5',
+                  f'\N{SUPERSCRIPT SIX}' : '^6',
+                  f'\N{SUPERSCRIPT SEVEN}' : '^7',
+                  f'\N{SUPERSCRIPT EIGHT}' : '^8',
+                  f'\N{SUPERSCRIPT NINE}' : '^9'}
+unicode_super_digits = {'\u2070'           : '0',
+                        f'\N{DEGREE SIGN}' : '0',
+                        f'\N{SUPERSCRIPT ONE}' : '1',
+                        f'\N{SUPERSCRIPT TWO}' : '2',
+                        f'\N{SUPERSCRIPT THREE}': '3',
+                        f'\N{SUPERSCRIPT FOUR}': '4',
+                        f'\N{SUPERSCRIPT FIVE}': '5',
+                        f'\N{SUPERSCRIPT SIX}' : '6',
+                        f'\N{SUPERSCRIPT SEVEN}' : '7',
+                        f'\N{SUPERSCRIPT EIGHT}' : '8',
+                        f'\N{SUPERSCRIPT NINE}' : '9'}
+
+
+unicode_subs = {f'\N{SUBSCRIPT ZERO}' : '_0',
+                f'\N{SUBSCRIPT ONE}' : '_1',
+                f'\N{SUBSCRIPT TWO}' : '_2',
+                f'\N{SUBSCRIPT THREE}': '_3',
+                f'\N{SUBSCRIPT FOUR}': '_4',
+                f'\N{SUBSCRIPT FIVE}': '_5',
+                f'\N{SUBSCRIPT SIX}' : '_6',
+                f'\N{SUBSCRIPT SEVEN}' : '_7',
+                f'\N{SUBSCRIPT EIGHT}' : '_8',
+                f'\N{SUBSCRIPT NINE}' : '_9'}
+
+unicode_sub_digits = {f'\N{SUBSCRIPT ZERO}' : '0',
+                      f'\N{SUBSCRIPT ONE}' : '1',
+                      f'\N{SUBSCRIPT TWO}' : '2',
+                      f'\N{SUBSCRIPT THREE}': '3',
+                      f'\N{SUBSCRIPT FOUR}': '4',
+                      f'\N{SUBSCRIPT FIVE}': '5',
+                      f'\N{SUBSCRIPT SIX}' : '6',
+                      f'\N{SUBSCRIPT SEVEN}' : '7',
+                      f'\N{SUBSCRIPT EIGHT}' : '8',
+                      f'\N{SUBSCRIPT NINE}' : '9'}
+
+unicode_specials = {'\u20D7' : '\\vec'}
+
+
+def replace_special_chars(string):
+    for i, char in enumerate(string):
+        if char in unicode_specials.keys():
+            tmp = string[:i] + unicode_specials[char] + string[i:]
+            string = tmp
+    return string
+
+
+def replace_superscripts(string):
+    N = len(string)
+    cpy = ''
+    for i,char in enumerate(string):
+        if char in unicode_supers.keys():
+            if i == N-1:
+                cpy += unicode_supers[char]
+            else:
+                making_num = True
+                num = unicode_super_digits[char]
+                j = i+1
+                while j < N and making_num:
+                    nextchar = string[j]
+                    if nextchar in unicode_supers.keys():
+                        num += unicode_super_digits[nextchar]
+                        j += 1
+                    else:
+                        making_num = False
+                if j == i + 1:
+                    num = unicode_supers[char]
+
+        else:
+            cpy += char
+
+    return cpy
+
+def replace_subscripts(string):
+    N = len(string)
+    cpy = ''
+    for i,char in enumerate(string):
+        if char in unicode_subs.keys():
+            if i == N-1:
+                cpy += unicode_subs[char]
+            else:
+                making_num = True
+                num = unicode_sub_digits[char]
+                j = i+1
+                while j < N and making_num:
+                    nextchar = string[j]
+                    if nextchar in unicode_subs.keys():
+                        num += unicode_sub_digits[nextchar]
+                        j += 1
+                    else:
+                        making_num = False
+        else:
+            cpy += char
+
+    return cpy
+
+class Equation(Environment):
+
+    #__slots__ = ['ufl_handler']
+
+    def __init__(self, math_text_lines: Iterable, starred: bool=False, eq_name: str=None):
+        
+        req_pkgs = [UsePackage('amsmath'),UsePackage('amsfonts')]
+        super().__init__('equation',math_text_lines,eq_name if eq_name else 'Equation',
+                         starred,env_options=None,required_packages=req_pkgs)
+
+        #self.ufl_handler = None
+
+    @staticmethod
+    def from_ufl(ufl_expr, starred: bool=False, eq_name: str=None):
+        from ufl.formatting.ufl2unicode import ufl2unicode
+        from pylatexenc.latexencode import unicode_to_latex
+        uni = ufl2unicode(ufl_expr)
+        uni = replace_superscripts(uni)
+        uni = replace_subscripts(uni)
+
+        lines = unicode_to_latex(uni,unknown_char_policy='ignore')
+        
+        code = lines.replace('[i]','_{i}')
+        code = code.replace('[i,i]','_{ii}')
+        code = code.replace('[i,i,i]','_{iii}')
+        code = code.replace('\\ensuremath{\\forall} i,i','')
+        code = code.replace('\\ensuremath{\\forall} i','')
+        code = code.replace('{\\textasciicircum}','^')
+        code = code.replace('{\\textunderscore}','_')
+        code = code.replace('[rest of domain]','_{\\Omega}')
+        code = code.replace('\\_','_')
+
+        return Equation([code],starred,eq_name)
+        
+
 class Section(TextLines):
 
     __slots__ = ['begin']
